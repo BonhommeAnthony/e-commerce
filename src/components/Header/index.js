bbf712be42013a5fa91e62b0cf4a8a65f0ce0d6f
Anthony Bonhomme <61671988+BonhommeAnthony@users.noreply.github.com>
2020-06-09 16:58:41 +0900

    initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="wrap">
+        <div className="logo">
+          <h1>AB SALE</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
