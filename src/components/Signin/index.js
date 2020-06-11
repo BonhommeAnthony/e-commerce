babe1ea4e6577c02644ee478bcd1e07a59b88e55
Anthony Bonhomme <61671988+BonhommeAnthony@users.noreply.github.com>
2020-06-11 21:11:19 +0900

    correction erreur commit

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -3,10 +3,7 @@ import "./styles.scss";
 import Button from "../Forms/Button";
 import { signInWithGoogle, auth } from "./../../firebase/utils";
 import FormInput from "../FormInput";
-<<<<<<< HEAD
 import AuthWrapper from "../AuthWrapper";
-=======
->>>>>>> d34a434b8969404e82589add63016405b9f27bc2
 
 const Signin = () => {
   const [state, setstate] = useState({ email: "", password: "" });
@@ -32,21 +29,15 @@ const Signin = () => {
     } catch (err) {
       // ("console.log(err);");
     }
-<<<<<<< HEAD
   };
 
   const { email, password } = state;
 
   const configAuthWrapper = {
     headline: "LogIn",
-=======
->>>>>>> d34a434b8969404e82589add63016405b9f27bc2
   };
 
-  const { email, password } = state;
-
   return (
-<<<<<<< HEAD
     <AuthWrapper {...configAuthWrapper}>
       <div className="formWrap">
         <form onSubmit={handleSubmit}>
@@ -69,33 +60,6 @@ const Signin = () => {
           <div className="socialSignin">
             <div className="row">
               <Button onClick={signInWithGoogle}>Sign in with Google</Button>
-=======
-    <div className="signin">
-      <div className="wrap">
-        <h2>Login</h2>
-        <div className="formWrap">
-          <form onSubmit={handleSubmit}>
-            <FormInput
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Email"
-              handleChange={handleChange}
-            />
-            <FormInput
-              type="password"
-              name="password"
-              value={password}
-              placeholder="Password"
-              handleChange={handleChange}
-            />
-            <Button type="submit">Sign in </Button>
-
-            <div className="socialSignin">
-              <div className="row">
-                <Button onClick={signInWithGoogle}>Sign in with Google</Button>
-              </div>
->>>>>>> d34a434b8969404e82589add63016405b9f27bc2
             </div>
           </div>
         </form>
